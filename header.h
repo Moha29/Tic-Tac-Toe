c547f9aafb9f9e4a6fbfede46a56bb31fab8c924
Moha29 <81183253+Moha29@users.noreply.github.com>
2021-12-23 19:07:33 -0800

    Create header.h

---
+++ header.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>   
+#include <string.h>
+using namespace std;
+
+//This is a game of Tic-Tac-Toe, where the user is facing a computer with random inputs.
+
+int check(char choices[], char input);
+int enter(char choices[], char input);
